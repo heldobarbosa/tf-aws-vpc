333da95a452f61d33913b8779f074ffc622ac206
Heldo <38962212+heldobarbosa@users.noreply.github.com>
2021-03-29 18:20:13 -0300

    change name of bucket

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     # Lembre de trocar o bucket para o seu, não pode ser o mesmo nome
-    bucket = "bucketheldo0.1"
+    bucket = "bucketheldo0.2"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
